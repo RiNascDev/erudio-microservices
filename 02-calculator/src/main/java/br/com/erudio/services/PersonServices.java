bfca7cc8fab7c9d723acb04d55291c435f194f8a
Rita C L Nascimento <sarahsetej@gmail.com>
2022-02-11 03:49:53 -0300

    Person end

--- 02-calculator/src/main/java/br/com/erudio/services/PersonServices.java
+++ 02-calculator/src/main/java/br/com/erudio/services/PersonServices.java
@@ -1,5 +1,7 @@
 package br.com.erudio.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.stereotype.Service;
@@ -11,6 +13,18 @@ public class PersonServices {
 	
 	private final AtomicLong counter = new AtomicLong();
 	
+	public Person create(Person person) {
+		return person;
+	}
+	
+	public Person update(Person person) {
+		return person;
+	}
+	
+	public void delete (String id) {
+		
+	}
+	
 	public Person findById(String id) {
 		Person person = new Person();
 		person.setId(counter.incrementAndGet());
@@ -19,6 +33,25 @@ public class PersonServices {
 		person.setAddress("Montes Claros - MG - Brasil");
 		person.setGender("female");
 		return person ;
+	}
+	
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<Person>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;
+	}
+
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId((long) i);
+		person.setFirstName("Person name" + i);
+		person.setLastName("Last name"+ i);
+		person.setAddress("Some address in Brasil" +i);
+		person.setGender("female");
+		return person;
 	}
 	
 
